2f9e4ccdc28f47f779f7e9584347588eab95872b
darius <dariusa.bualanjr@pccw.com>
2020-09-17 01:07:16 +0800

    initial commit

---
+++ src/AppRoot.js
@@ -0,0 +1,5 @@
+<template>
+  <h1>App Root</h1>
+</template>
+
+export default class AppRoot { }
---
+++ src/main.js
@@ -0,0 +1,7 @@
+import { Module } from 'munster';
+import AppRoot from './AppRoot';
+
+new Module({
+  selector: 'app-root',
+  component: AppRoot
+});
